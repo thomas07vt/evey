fdc47e5b0867cffed0dc08b55c43321de92d2e5b
John Thomas <thomas07@vt.edu>
2021-04-28 13:12:41 -0400

    add uuid to serializer

--- lib/evey/event_serializer.rb
+++ lib/evey/event_serializer.rb
@@ -1,6 +1,6 @@
 class Evey::EventSerializer < Kix::Serializer
   attributes :id, :type, :data, :metadata, :aggregates, :associations,
-    :created_at, :updated_at, :errors
+    :created_at, :updated_at, :uuid, :errors,
 
   def errors
     throw(:skip) if _object.errors.empty?
